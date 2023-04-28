430f6799d5dca8ec01f96ea3ff5d6871d9334bc8
Nebiyyeh Dagdeviren <nebiyyehdagdeviren@gmail.com>
2023-04-28 07:55:43 -0500

    router and axios added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import ListEmployeeComponent from './components/ListEmployeeComponent';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
-    <div className="container">
-        <ListEmployeeComponent />
+    <div>
+      <Router>
+          <HeaderComponent />
+          <div className="container">
+          <ListEmployeeComponent />
+            <Routes>
+              <Route path="/" component={ListEmployeeComponent}></Route>
+              <Route
+                path="/employees"
+                component={ListEmployeeComponent}
+              ></Route>
+
+            </Routes>
+          </div>
+          <FooterComponent />
+      </Router>
     </div>
   );
 }
